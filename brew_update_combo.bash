21be52267669f0db5c5113e8c552eea2c32476e5
hmr@entropy3 <hmr@entropy3.local>
2021-03-11 02:31:02 +0900

    Inintial import.

---
+++ brew_update_combo.bash
@@ -0,0 +1,53 @@
+#!/usr/local/bin/bash
+
+# brew_update_combo.bash
+# One touch solution to upgrade Homebrew formulae and casks.
+# https://github.com/hmr/brew_update_combo
+
+function t_echo () {
+  echo "----------------------------------------"
+  echo "-> $1"
+  echo "----------------------------------------"
+}
+
+t_echo "Updating BREW..."
+brew update
+echo
+
+t_echo "Outdated formulae are..."
+OUTDATED=$(brew outdated | gwc -l)
+if gtest ${OUTDATED} -eq 0; then
+  echo "None."
+  echo
+else
+  brew outdated --formula --verbose
+  echo
+
+  t_echo "Upgrading formulae..."
+  brew upgrade --formula
+  echo
+fi
+
+t_echo "Outdated casks are..."
+OUTDATED_CASK=$(brew outdated --cask | gwc -l)
+if gtest ${OUTDATED_CASK} -eq 0; then
+  echo "None."
+  echo
+else
+  brew outdated --cask --greedy --verbose
+  #brew cask outdated
+  echo
+
+  t_echo "Upgrading casks..."
+  brew upgrade --cask --greedy
+  echo
+fi
+
+if gtest ${OUTDATED} -ne 0 -o ${OUTDATED_CASK} -ne 0; then
+  t_echo "Cleaning up old files..."
+  brew cleanup -s
+  echo
+fi
+
+echo "DONE!"
+
